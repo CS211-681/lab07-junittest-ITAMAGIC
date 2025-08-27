4926dae560fa4410cc268fbef5ae4fd88234ea13
Messiah <sirapob.no@ku.th>
2025-08-27 19:28:50 +0700

    testers

--- src/test/java/ku/cs/models/StudentListTest.java
+++ src/test/java/ku/cs/models/StudentListTest.java
@@ -1,4 +1,51 @@
+package ku.cs.models;
+
 import static org.junit.jupiter.api.Assertions.*;
+import ku.cs.services.StudentListHardCodeDatasource;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class StudentListTest {
-  
+
+    private StudentList slist;
+
+    @BeforeEach
+    void setUp(){
+        StudentListHardCodeDatasource datasource = new StudentListHardCodeDatasource();
+        slist = datasource.readData();
+    }
+
+    @Test
+    @DisplayName("เพิ่มนักเรียนไม่มีคะแนน + ลอง filter")
+    void testAddNoScore(){
+        slist.addNewStudent("6710400007", "Primmie");
+        assertNotNull(slist.findStudentById("6710400007"));
+
+        slist.addNewStudent("6710400007", "Plammy");
+        StudentList filter = slist.filterByName("Plammy");
+        assertNull(filter.findStudentById("6710400007"));
+    }
+
+    @Test
+    @DisplayName("เพิ่มนักเรียนมีคะแนน")
+    void testAddWithScore(){
+        slist.addNewStudent("6710400006", "Primmie", 130);
+        assertNotNull(slist.findStudentById("6710400006"));
+    }
+
+    @Test
+    @DisplayName("หาด้วย ID")
+    void testFindByID(){
+        assertNotNull(slist.findStudentById("6710400009"));
+        assertNull(slist.findStudentById("0123456789"));
+    }
+
+    @Test
+    @DisplayName("ให้คะแนนแล้วดูเกรด")
+    void testGiveScoreAndGrade(){
+        slist.addNewStudent("6710400007", "Primmie", 30);
+        slist.giveScoreToId("6710400007", 50);
+        assertNotNull(slist.viewGradeOfId("6710400007"));
+    }
 }
--- src/test/java/ku/cs/models/StudentTest.java
+++ src/test/java/ku/cs/models/StudentTest.java
@@ -1,4 +1,51 @@
+package ku.cs.models;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class StudentTest {
-  
+
+    @Test
+    @DisplayName("ทดสอบระบบ isID")
+    void testIsID() {
+        Student s = new Student("6710451399", "SirapobNoomthong");
+        assertTrue(s.isId("6710451399"));
+    }
+
+    @Test
+    @DisplayName("ทดสอบการเปลี่ยนชื่อ")
+    void testChangeName() {
+        Student s = new Student("6710451399", "SirapobNoomthong");
+        s.changeName("GehrmanSparrow");
+        assertEquals("GehrmanSparrow", s.getName());
+    }
+
+    @Test
+    @DisplayName("ทดสอบว่าชื่อตรงมั้ย")
+    void testIsContains(){
+        Student s = new Student("6710451399", "SirapobNoomthong");
+        assertTrue(s.isNameContains("Sirapob"));
+    }
+
+    @Test
+    @DisplayName("ทดสอบการเพิ่มคะแนน 50.5 คะแนน")
+    void testAddScore(){
+        Student s = new Student("6710451399", "SirapobNoomthong");
+        s.addScore(50.5);
+        assertEquals(50.5, s.getScore());
+
+    }
+
+    @Test
+    @DisplayName("ทดสอบการเพิ่มคะแนน 85 คะแนน และให้ Object คำนวนเกรดออกมา")
+    void testCalculateGrade(){
+        Student s = new Student("6710451399", "SirapobNoomthong");
+        s.addScore(85);
+        assertEquals("A", s.getGrade());
+    }
+
+
+
 }
